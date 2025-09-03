3928384d4707d9d17d44dc8bb303227be7fd72ce
AlinaD-2912 <dvornyk.alina2912@gmail.com>
2025-09-03 13:41:44 +0200

    test product's quality and sellin decreasing failed

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -3,6 +3,7 @@ package com.magasin;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MagasinTest {
 
@@ -13,5 +14,18 @@ class MagasinTest {
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
+
+    @Test
+    void normalProduct_decreaseQualityAndSellin() {
+        Item[] items = new Item[] { new Item("Normal product", 10, 20) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+
+        assertEquals("Normal product", app.items[0].name);
+        assertEquals(19, app.items[0].sellIn); // 20 - 1 = 19
+        assertEquals(9, app.items[0].quality); // 10 - 1 = 9
+    }
+
+
 
 }
